8924dd032909a31bb83662a585a327438354dc87
Stacey <staceynunez0@gmail.com>
2018-10-10 10:10:18 +0200

    Changes

--- contact-mail.php
+++ contact-mail.php
@@ -1,22 +1,24 @@
 <?php
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = strip_tags(trim($_POST["FullName"]));
+        $name = strip_tags(trim($_POST["namesurname"]));
 		$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $phone = strip_tags(trim($_POST["Phone"]));
-        $email = filter_var(trim($_POST["Email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["Message"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["description"]);
+        $youtubelink = trim($_POST["youtube"]);
+        $instagramlink = trim($_POST["instagram"]);
         if ( empty($name)  OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";/*--------- Contact submission erroe Message ---------------*/
             exit;
         }
-        $recipient = "michel@statelygroup.co"; /*----- Add your email address here------*/
+        $recipient = "nunez.s@statelygroup.co"; /*----- Add your email address here------*/
         $subject = "Your Subject $name";/*------ Add your email subject here------*/
         $email_content = "Name: $name";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Phone: $phone\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content .= "email: $email\n\n";
+        $email_content .= "youtube: $youtubelink\n\n";
+        $email_content .= "instagram: $instagramlink\n\n";
+        $email_content .= "description:\n$message\n";
         $email_headers = "From: $name <$email>";
         if (mail($recipient, $subject, $email_content, $email_headers)) {
 
